ec6071188b8d6ea086caff1e971001f612f043a8
sganzha81 <sganzha@gmail.com>
2026-04-25 00:49:23 +0400

    Add function input from user

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+formula = input("Enter function of x: ")
+
 x = np.linspace(-10, 10, 100)
-y = x ** 2
+y = eval(formula)
 
 plt.plot(x, y)
-plt.title("Graph of y = x^2")
+plt.title(f"Graph of y = {formula}")
 plt.xlabel("x")
 plt.ylabel("y")
 plt.grid(True)
