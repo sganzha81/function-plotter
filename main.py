80c2ab4e668f0e6d20cba78b998ad7bb31317785
sganzha81 <sganzha@gmail.com>
2026-04-25 20:55:07 +0400

    Add error handling for invalid formulas and sqrt of negative numbers

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ from numpy import sin, cos, sqrt
 formula = input("Enter function of x: ")
 
 x = np.linspace(-10, 10, 100)
-y = eval(formula)
-
-plt.plot(x, y)
-plt.title(f"Graph of y = {formula}")
-plt.xlabel("x")
-plt.ylabel("y")
-plt.grid(True)
-
-plt.show()
+try:
+    y = eval(formula)
+except:
+    print("Ошибка: формула введена неправильно")
+    print("Примеры правильных формул: x**2, sin(x), x**2 + 3*x - 1")
+else:
+    plt.plot(x, y)
+    plt.title(f"Graph of y = {formula}")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.grid(True)
+    plt.show()
